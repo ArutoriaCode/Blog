fd4ba4ca22fd77faff0c742a1331da53ae05e7a0
Arutoria <17620725494@163.com>
2020-11-15 21:57:03 +0800

    Revert "[*]首页轮播图样式完成"
    This reverts commit a5fe48fa2e9e34fb0e3fd21c346c76355eafb86d.

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -3,11 +3,8 @@ import Vue from 'vue'
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from '..\\layouts\\error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '..\\assets\\index.scss'
-
-import '..\\node_modules\\vuetify\\dist\\vuetify.css'
 
 import _6f6c098b from '..\\layouts\\default.vue'
 
@@ -46,7 +43,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -182,10 +179,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
--- components/ArticlesCard.vue
+++ components/ArticlesCard.vue
@@ -4,7 +4,7 @@
 
     <v-card-title> 也许这就是人生吧 </v-card-title>
 
-    <v-card-subtitle> 这是一段静态的文本 </v-card-subtitle>
+    <v-card-subtitle> 特朗普丢掉了大选的胜利 </v-card-subtitle>
 
     <v-card-actions>
       <v-btn color="error" text> 开始阅读 </v-btn>
