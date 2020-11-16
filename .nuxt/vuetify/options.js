2ca51dd91403bd5096894cfd8c94af64b773852c
Arutoria <17620725494@163.com>
2020-11-16 14:06:57 +0800

    delete .nuxt

--- .nuxt/vuetify/options.js
+++
@@ -1,1 +0,0 @@
-export default {"theme":{"themes":{"dark":{"primary":"#1976d2","accent":"#424242","secondary":"#ff8f00","info":"#26a69a","warning":"#ffc107","error":"#dd2c00","success":"#00e676","saber":"#22469a","elaina":"#f1e8eb"},"light":{"saber":"#22469a","elaina":"#f1e8eb"}}},"breakpoint":{"mobileBreakpoint":1264}}
